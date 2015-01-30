a089022d971be0e3a7097f403cb0f2a58cfe54ac
Chris Done <chrisdone@gmail.com>
2015-01-30 05:40:47 +0100

    More payload variation

--- gmail-thread-mode.el
+++ gmail-thread-mode.el
@@ -61,8 +61,8 @@
                                   labels)))
       (insert (propertize subject
                           'face (if unread
-                                    'gmail-thread-mode-subject-unread-face
-                                  'gmail-thread-mode-subject-face))
+                                    'gmail-search-mode-subject-unread-face
+                                  'gmail-search-mode-subject-face))
               " "
               (propertize
                (format-time-string gmail-search-mode-date-format date)
@@ -123,25 +123,29 @@
 
 (defun gmail-thread-mode-plaintext (payload)
   "Get the plain text from a message payload."
-  (let ((plain (car (remove-if-not (lambda (part)
-                                     (string-prefix-p "text/plain" (plist-get part :mimeType)))
-                                   (plist-get payload :parts)))))
-    (if plain
-        (gmail-encoding-decode-base64
-         (plist-get (plist-get plain :body) :data))
-      (let ((multipart (car (remove-if-not (lambda (part)
-                                             (string-prefix-p "multipart/alternative" (plist-get part :mimeType)))
-                                           (plist-get payload :parts)))))
-        (if multipart
-            (let ((plain (car (remove-if-not (lambda (part)
-                                               (string-prefix-p "text/plain" (plist-get part :mimeType)))
-                                             (plist-get multipart :parts)))))
-              (if plain
-                  (gmail-encoding-decode-base64 (plist-get (plist-get plain :body) :data))
-                (progn (message "Unable to find a plain/text field from this email, even in multi-parts.")
-                       "")))
-          (progn (message "Unable to find a plain/text field from this email.")
-                 ""))))))
+  (if (string-prefix-p "text/plain" (plist-get payload :mimeType))
+      (gmail-encoding-decode-base64
+       (plist-get (plist-get payload :body) :data))
+    (let ((plain (car (remove-if-not (lambda (part)
+                                       (string-prefix-p "text/plain" (plist-get part :mimeType)))
+                                     (plist-get payload :parts)))))
+      (if plain
+          (gmail-encoding-decode-base64
+           (plist-get (plist-get plain :body) :data))
+        (let ((multipart (car (remove-if-not (lambda (part)
+                                               (string-prefix-p "multipart/alternative" (plist-get part :mimeType)))
+                                             (plist-get payload :parts)))))
+          (if multipart
+              (let ((plain (car (remove-if-not (lambda (part)
+                                                 (string-prefix-p "text/plain" (plist-get part :mimeType)))
+                                               (plist-get multipart :parts)))))
+                (if plain
+                    (gmail-encoding-decode-base64 (plist-get (plist-get plain :body) :data))
+                  (progn (message "Unable to find a plain/text field from this email, even in multi-parts: %S" payload)
+                         "")))
+            (progn (message "Unable to find a plain/text field from this email: %S" payload)
+                   "")))))
+    ))
 
 (defun gmail-thread-mode-fill-lines (start end)
   "Fill lines longer than 80 columns, to make it more readable."
